99f7c8263d80d9c4a569a9b7596431f78216dbf5
Michael Vorburger <mike@vorburger.ch>
2014-05-10 03:28:28 +0200

    Architecture outline v0.0.1

--- mifosng-provider/src/main/java/org/mifosplatform/infrastructure/sms/scheduler/ScheduledSendSMSJobServiceImpl.java
+++ mifosng-provider/src/main/java/org/mifosplatform/infrastructure/sms/scheduler/ScheduledSendSMSJobServiceImpl.java
@@ -8,7 +8,6 @@ package org.mifosplatform.infrastructure.sms.scheduler;
 import org.mifosplatform.infrastructure.jobs.annotation.CronTarget;
 import org.mifosplatform.infrastructure.jobs.exception.JobExecutionException;
 import org.mifosplatform.infrastructure.jobs.service.JobName;
-import org.mifosplatform.scheduledjobs.service.ScheduledJobRunnerServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -18,26 +17,26 @@ import org.springframework.transaction.annotation.Transactional;
 public class ScheduledSendSMSJobServiceImpl implements ScheduledSendSMSJobService {
     private final static Logger logger = LoggerFactory.getLogger(ScheduledSendSMSJobServiceImpl.class);
 
-	@Override
-	@Transactional
-	@CronTarget(jobName = JobName.POLL_AND_SEND_PENDING_SMS)
-	public void pollDBAndSendPendingSMSToGateway() throws JobExecutionException {
-		System.out.println("pollDBAndSendPendingSMSToGateway println");
-		logger.error("pollDBAndSendPendingSMSToGateway logger.error");
-		
-		// TODO Implementation...
-		// Find chunks of messages in status PENDING,
-		//    and push them into SmsGateway.send()
-		//    and set their status to SENT
-	}
-	
-//	@Override
-//	@Transactional
-//	@CronTarget(jobName = JobName.UPDATE_SMS_DELIVERY_STATUS)
-//	public void pollDBAndUpdateDeliveryStatus() throws JobExecutionException {
-//		// TODO Implementation...
-//		// Find chunks of messages in status SENT,
-//		//    and check if SmsGateway.getDeliveryStatus() has any news about them
-//		//    and, if yes, update their resp. status to either DELIVERED or FAILED
-//	}
+        @Override
+        @Transactional
+        @CronTarget(jobName = JobName.POLL_AND_SEND_PENDING_SMS)
+        public void pollDBAndSendPendingSMSToGateway() throws JobExecutionException {
+                System.out.println("pollDBAndSendPendingSMSToGateway println");
+                logger.error("pollDBAndSendPendingSMSToGateway logger.error");
+                
+                // TODO Implementation...
+                // Find chunks of messages in status PENDING,
+                //    and push them into SmsGateway.send()
+                //    and set their status to SENT
+        }
+        
+//      @Override
+//      @Transactional
+//      @CronTarget(jobName = JobName.UPDATE_SMS_DELIVERY_STATUS)
+//      public void pollDBAndUpdateDeliveryStatus() throws JobExecutionException {
+//              // TODO Implementation...
+//              // Find chunks of messages in status SENT,
+//              //    and check if SmsGateway.getDeliveryStatus() has any news about them
+//              //    and, if yes, update their resp. status to either DELIVERED or FAILED
+//      }
 }
