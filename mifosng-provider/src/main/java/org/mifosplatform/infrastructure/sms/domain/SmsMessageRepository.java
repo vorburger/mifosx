57bfea049e9427d0e077d5be31aacf37061f81d0
Michael Vorburger <mike@vorburger.ch>
2014-05-11 16:54:52 +0200

    yuhu - working table reading ;)

--- mifosng-provider/src/main/java/org/mifosplatform/infrastructure/sms/domain/SmsMessageRepository.java
+++ mifosng-provider/src/main/java/org/mifosplatform/infrastructure/sms/domain/SmsMessageRepository.java
@@ -5,9 +5,18 @@
  */
 package org.mifosplatform.infrastructure.sms.domain;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SmsMessageRepository extends JpaRepository<SmsMessage, Long>, JpaSpecificationExecutor<SmsMessage> {
-    // no extra behaviour
+
+    // TODO how to? @Query("from SmsMessage msg where msg.statusType = " +
+    // SmsMessageStatusType.PENDING.getValue() + " order by msg.id")
+    @Query("from SmsMessage msg where msg.statusType = 100 order by msg.id")
+    List<SmsMessage> findPending(Pageable pageable);
+
 }
