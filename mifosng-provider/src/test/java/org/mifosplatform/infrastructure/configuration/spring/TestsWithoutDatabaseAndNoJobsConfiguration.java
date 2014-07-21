83cb73229ed1b6fd9a8310e3f3c9b07ea296412d
Michael Vorburger <mike@vorburge>
2014-07-21 22:39:05 +0200

    New SpringConfigurationTest, the first @RunWith(SpringJUnit4ClassRunner.class) @ContextConfiguration ... in Mifos X. More like this to come - stay tuned.
    Signed-off-by: Michael Vorburger <mike@vorburge>

---
+++ mifosng-provider/src/test/java/org/mifosplatform/infrastructure/configuration/spring/TestsWithoutDatabaseAndNoJobsConfiguration.java
@@ -0,0 +1,51 @@
+package org.mifosplatform.infrastructure.configuration.spring;
+
+import org.mifosplatform.infrastructure.core.service.TenantDatabaseUpgradeService;
+import org.mifosplatform.infrastructure.jobs.service.JobRegisterService;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Spring @Configuration which does not require a running database.
+ * It also does not load any job configuration (as they are in the DB), thus nor
+ * starts any background jobs. For some integration tests, this may be
+ * perfectly sufficient (and faster to run such tests).
+ */
+@Configuration
+// We do NOT want the testContext.xml here, but the real one!
+@ImportResource("classpath*:META-INF/spring/appContext.xml")
+public class TestsWithoutDatabaseAndNoJobsConfiguration {
+
+    /**
+     * Override TenantDatabaseUpgradeService binding, because the real
+     * one has a @PostConstruct upgradeAllTenants() which accesses
+     * the database on start-up.
+     */
+    @Bean
+    public TenantDatabaseUpgradeService tenantDatabaseUpgradeService() {
+        return new TenantDatabaseUpgradeService(null) {
+            @Override
+            public void upgradeAllTenants() {
+                // NOOP
+            }
+        };
+    }
+
+    /**
+     * Override JobRegisterService binding, because the real
+     * JobRegisterServiceImpl has a @PostConstruct loadAllJobs() which accesses
+     * the database on start-up.
+     */
+    @Bean
+    public JobRegisterService jobRegisterServiceImpl() {
+        JobRegisterService mockJobRegisterService = Mockito.mock(JobRegisterService.class);
+        return mockJobRegisterService;
+    }
+
+    // Another @Bean that would be sensible to mock in this @Configuration (with a separate test) would be:
+    // @Bean public DataSource tenantDataSourceJndi() {
+    // so that it throws a clear IllegalStateException("DataSource not available with TestSpringConfigurationWithoutDatabaseAndNoJobs, use TestSpringConfiguration if you need a DB (or override the @Bean requiring a DB at start-up if you don't)")
+}
