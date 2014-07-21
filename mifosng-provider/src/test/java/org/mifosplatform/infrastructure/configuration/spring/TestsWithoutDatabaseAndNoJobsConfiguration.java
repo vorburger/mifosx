5ac252e0216059b353035f6cbbbad8fb489340e7
Michael Vorburger <mike@vorburge>
2014-07-21 22:43:09 +0200

    SpringConfigurationTest approach extension for @RunWith(SpringJUnit4ClassRunner.class) which do require a real running database
    Signed-off-by: Michael Vorburger <mike@vorburge>

--- mifosng-provider/src/test/java/org/mifosplatform/infrastructure/configuration/spring/TestsWithoutDatabaseAndNoJobsConfiguration.java
+++ mifosng-provider/src/test/java/org/mifosplatform/infrastructure/configuration/spring/TestsWithoutDatabaseAndNoJobsConfiguration.java
@@ -15,9 +15,7 @@ import org.springframework.context.annotation.ImportResource;
  * perfectly sufficient (and faster to run such tests).
  */
 @Configuration
-// We do NOT want the testContext.xml here, but the real one!
-@ImportResource("classpath*:META-INF/spring/appContext.xml")
-public class TestsWithoutDatabaseAndNoJobsConfiguration {
+public class TestsWithoutDatabaseAndNoJobsConfiguration extends TestSpringConfiguration {
 
     /**
      * Override TenantDatabaseUpgradeService binding, because the real
