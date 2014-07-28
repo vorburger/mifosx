c5bc5100298bb887ba75316096f5853a06442fef
Michael Vorburger <mike@vorburger.ch>
2014-07-28 20:52:25 +0200

    Spring Boot POC (not fully working, yet)
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

---
+++ mifosng-provider/src/main/java/org/mifosplatform/MifosConfiguration.java
@@ -0,0 +1,9 @@
+package org.mifosplatform;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+public class MifosConfiguration {
+
+}
---
+++ mifosng-provider/src/main/java/org/mifosplatform/MifosServerApplication.java
@@ -0,0 +1,18 @@
+package org.mifosplatform;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Mifos main() launching embedded HTTP server.
+ *
+ * You can easily launch this debug - without needing command line Gradle stuff remote attachment etc.
+ *
+ * It's the old/classic Mifos (non-X) Workspace 2.0 reborn! ;-)
+ */
+public class MifosServerApplication {
+
+	public static void main(String[] args) throws Exception {
+        SpringApplication.run(MifosConfiguration.class, args);
+    }
+
+}
