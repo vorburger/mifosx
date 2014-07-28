d125bdc30f959bebb80a4390f91de5a9fe0c49ec
Michael Vorburger <mike@vorburger.ch>
2014-07-29 00:39:26 +0200

    Spring Boot now with 'real' DataSource
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

---
+++ mifosng-provider/src/test/java/org/mifosplatform/infrastructure/configuration/spring/DataSourceConfigurationTest.java
@@ -0,0 +1,32 @@
+package org.mifosplatform.infrastructure.configuration.spring;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mifosplatform.DataSourceConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = DataSourceConfiguration.class)
+public class DataSourceConfigurationTest {
+
+    @Autowired
+    @Qualifier("tenantDataSourceJndi")
+    DataSource dataSource;
+
+    @Test
+    public void testTestDatasource() throws Exception {
+        Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement();
+        statement.execute("select 1");
+        statement.close();
+        connection.close();
+    }
+}
