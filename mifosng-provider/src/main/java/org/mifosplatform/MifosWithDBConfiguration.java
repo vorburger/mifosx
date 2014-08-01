c1f63f9baf3d21dca832c46f0f80caf4f49cb9e9
Michael Vorburger <mike@vorburger.ch>
2014-08-02 01:56:30 +0200

    WIP re. MariaDB4j integration (INCOMPLETE) - yay, it starts up!! Remaining problems: 1. Non-3306 port (Tenant DB), 2. NoSuchMethodError: org.hamcrest.Matcher.describeMismatch(Ljava/lang/Object;Lorg/hamcrest/Description;), 3. web.xml?
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

--- mifosng-provider/src/main/java/org/mifosplatform/MifosWithDBConfiguration.java
+++ mifosng-provider/src/main/java/org/mifosplatform/MifosWithDBConfiguration.java
@@ -7,6 +7,8 @@ package org.mifosplatform;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
@@ -14,7 +16,7 @@ import org.springframework.context.annotation.ImportResource;
 @Configuration
 @Import(MariaDB4jDataSourceConfiguration.class)
 @ImportResource("classpath*:META-INF/spring/appContext.xml")
-@EnableAutoConfiguration(exclude=DataSourceAutoConfiguration.class)
+@EnableAutoConfiguration(exclude={ DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 // NOTE: Due to https://github.com/spring-projects/spring-boot/issues/1328, we must repeat above, and cannot just extends MifosConfiguration and "override" only @Import(MariaDB4jDataSourceConfiguration.class)
 public class MifosWithDBConfiguration {
 }
