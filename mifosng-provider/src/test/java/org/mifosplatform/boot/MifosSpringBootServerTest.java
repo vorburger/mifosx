877d264fa2f09db57184624c225e781206913eb5
Michael Vorburger <mike@vorburger.ch>
2014-08-09 01:30:07 +0200

    WIP re. MariaDB4j integration (INCOMPLETE) - removed ensureNoSLF4jProblems() @Test - that doesn't work..
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

--- mifosng-provider/src/test/java/org/mifosplatform/boot/MifosSpringBootServerTest.java
+++ mifosng-provider/src/test/java/org/mifosplatform/boot/MifosSpringBootServerTest.java
@@ -1,17 +1,14 @@
 package org.mifosplatform.boot;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mifosplatform.MifosServerApplication;
 import org.mifosplatform.MifosWithDBConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.OutputCapture;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -34,9 +31,6 @@ public class MifosSpringBootServerTest {
 	@Value("${local.server.port}")
 	protected int httpPort;
 	
-	@Rule
-	public OutputCapture capture = new OutputCapture();
-	
 	protected RestTemplate template = new TestRestTemplate();
 
 	@Test
@@ -45,11 +39,6 @@ public class MifosSpringBootServerTest {
 		assertThat(response, containsString("\"username\": \"mifos\""));
 	}
 
-	@Test
-	public void ensureNoSLF4jProblems() {
-		assertThat(capture.toString(), not(containsString("SLF4J: Class path contains multiple SLF4J bindings")));	
-	}
-	
 	protected String getApiUrl(String trailingApiUrl) {
 		Preconditions.checkArgument(trailingApiUrl.startsWith("/"), "trailingApiUrl must start with slash: " + trailingApiUrl);
 		return "http://localhost:" + httpPort + "/api/v1" + trailingApiUrl;
