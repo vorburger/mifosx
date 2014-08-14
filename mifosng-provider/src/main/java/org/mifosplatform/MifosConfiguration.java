6cf3521d7bde3d1f7334511523a13b0122c8b1aa
Michael Vorburger <mike@vorburger.ch>
2014-08-15 00:58:28 +0200

    WebConfiguration instead of web.xml - and now it works!! really neat.
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

--- mifosng-provider/src/main/java/org/mifosplatform/MifosConfiguration.java
+++ mifosng-provider/src/main/java/org/mifosplatform/MifosConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@Import({ DataSourceConfiguration.class, TomcatSSLConfiguration.class })
+@Import({ WebConfiguration.class, DataSourceConfiguration.class, TomcatSSLConfiguration.class })
 @ImportResource("classpath*:META-INF/spring/appContext.xml")
 @EnableAutoConfiguration(exclude={ DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 public class MifosConfiguration {
