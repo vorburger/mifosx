0150855d182b2a8dc26f1324211f422f047eee33
Michael Vorburger <mike@vorburger.ch>
2014-08-15 01:36:49 +0200

    preparing for traditional WAR support (not yet tested), and making sure that there will be no confusion between web.xml vs. org.mifosplatform.WebConfiguration later
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

---
+++ mifosng-provider/src/main/java/org/mifosplatform/MifosServletWebApplicationInitializerConfiguration.java
@@ -0,0 +1,32 @@
+package org.mifosplatform;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Required to still have a working legacy "classic" WAR.
+ * 
+ * @see <a
+ *      href="http://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#howto-convert-an-existing-application-to-spring-boot">#howto-convert-an-existing-application-to-spring-boot</a>
+ */
+@Configuration
+@Import({ WebConfiguration.class })
+@ImportResource({ "classpath*:META-INF/spring/jndi.xml", "classpath*:META-INF/spring/appContext.xml" })
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class })
+public class MifosServletWebApplicationInitializerConfiguration extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // TODO switch between MifosConfiguration & MifosWithDBConfiguration
+        // respectively dynamically mix-in MariaDB4jDataSourceConfiguration
+        return application.sources(MifosServletWebApplicationInitializerConfiguration.class);
+    }
+}
