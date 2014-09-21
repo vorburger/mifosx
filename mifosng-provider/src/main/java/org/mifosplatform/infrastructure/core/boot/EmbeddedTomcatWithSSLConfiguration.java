fc03928fef7b30630ff22b4307b310a1884bf3ca
Michael Vorburger <mike@vorburger.ch>
2014-09-21 23:59:49 +0200

    Fixed the bug preventing keystore.jks from working in-WAR (if Resource is not a file:/ but on classpath:/)
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

--- mifosng-provider/src/main/java/org/mifosplatform/infrastructure/core/boot/EmbeddedTomcatWithSSLConfiguration.java
+++ mifosng-provider/src/main/java/org/mifosplatform/infrastructure/core/boot/EmbeddedTomcatWithSSLConfiguration.java
@@ -7,8 +7,10 @@ package org.mifosplatform.infrastructure.core.boot;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import org.apache.catalina.connector.Connector;
+import org.apache.commons.io.FileUtils;
 import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
@@ -38,7 +40,7 @@ public class EmbeddedTomcatWithSSLConfiguration {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
         try {
-            File keystore = getKeystore().getFile();
+            File keystore = getFile(getKeystore());
             File truststore = keystore;
             connector.setScheme("https");
             connector.setSecure(true);
@@ -55,7 +57,7 @@ public class EmbeddedTomcatWithSSLConfiguration {
         }
     }
 
-    protected int getHTTPSPort() {
+	protected int getHTTPSPort() {
 	// TODO This shouldn't be hard-coded here, but configurable
 		return 8443;
 	}
@@ -68,4 +70,35 @@ public class EmbeddedTomcatWithSSLConfiguration {
         return new ClassPathResource("/keystore.jks");
     }
 
+	public File getFile(Resource resource) throws IOException {
+		try {
+			return resource.getFile();
+		} catch (IOException e) {
+			// Uops.. OK, try again (below)
+		}
+
+		try {
+			URL url = resource.getURL();
+			// If this creates filenames that are too long on Win,
+			// then could just use resource.getFilename(),
+			// even though not unique, real risk prob. min.bon
+			String tempDir = System.getProperty("java.io.tmpdir");
+			tempDir = tempDir + "/" + getClass().getSimpleName() + "/";
+			String path = url.getPath();
+			String uniqName = path.replace("file:/", "").replace('!', '_');
+			String tempFullPath = tempDir + uniqName;
+			// instead of File.createTempFile(prefix?, suffix?);
+			File targetFile = new File(tempFullPath);
+			long len = resource.contentLength();
+			if (!targetFile.exists() || targetFile.length() != len) { // Only copy new files
+				FileUtils.copyURLToFile(url, targetFile);
+			}
+			return targetFile;
+		} catch (IOException e) {
+			// Uops.. erm, give up:
+			throw new IOException("Cannot obtain a File for Resource: "
+					+ resource.toString(), e);
+		}
+
+	}
 }
